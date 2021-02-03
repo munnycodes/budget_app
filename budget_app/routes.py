d450d416d6ceb4ececa2576a8e80b960aea0826b
munnytravels <munnawar.a.h@gmail.com>
2021-02-03 15:42:19 +0000

    registration now adds user data to db

--- budget_app/routes.py
+++ budget_app/routes.py
@@ -27,12 +27,15 @@ def posts():
 
 
 @app.route("/register", methods=['GET', 'POST'])
-@app.route("/register", methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        flash(f'Account created for {form.username.data}!', 'success')
-        return redirect(url_for('index'))
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash(f'Your account has been created, you can now log in!', 'success')
+        return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
 
 
