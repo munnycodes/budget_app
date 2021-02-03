cbcdb41bc7d3bc814f74d4173745caccdc9dffb8
munnytravels <munnawar.a.h@gmail.com>
2021-02-03 14:04:06 +0000

    update

--- budget_app/models.py
+++ budget_app/models.py
@@ -7,6 +7,7 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     username = db.Column(db.String(20), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
+    budgets = db.relationship('Budget', backref='author', lazy=True)
     
     def __repr__(self):
         return f"User('{self.username}', {self.email}"
@@ -18,7 +19,7 @@ class Budget(db.Model):
     balance_before = db.Column(db.Integer, nullable=False)
     balance_after = db.Column(db.Integer, nullable=False)
     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow) 
-
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     def __repr__(self):
         return f"User('{self.username}', {self.email}"
 
--- budget_app/routes.py
+++ budget_app/routes.py
@@ -8,8 +8,8 @@ from budget_app.models import User, Budget
 def index():
     return render_template('index.html')
 
-# @app.route('/posts', methods=['GET', 'POST'])
-# def posts():
+@app.route('/posts', methods=['GET', 'POST'])
+def posts():
 
 #     if request.method == 'POST': 
 #         budget_expense = request.form['expense']
@@ -23,35 +23,26 @@ def index():
 #         return redirect('/posts')
 #     else:
 #         all_posts = Budget.query.order_by(Budget.date_posted).all()
-#         return render_template('posts.html', posts=all_posts) 
+        return render_template('posts.html', posts=all_posts) 
 
 
 @app.route("/register", methods=['GET', 'POST'])
+@app.route("/register", methods=['GET', 'POST'])
 def register():
-    if current_user.is_authenticated:
-        return redirect(url_for('index'))
     form = RegistrationForm()
     if form.validate_on_submit():
-        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
-        db.session.add(user)
-        db.session.commit()
-        flash('Your account has been created! You are now able to log in', 'success')
-        return redirect(url_for('login'))
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('index'))
     return render_template('register.html', title='Register', form=form)
 
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
-    if current_user.is_authenticated:
-        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        user = User.query.filter_by(email=form.email.data).first()
-        if user and bcrypt.check_password_hash(user.password, form.password.data):
-            login_user(user, remember=form.remember.data)
-            next_page = request.args.get('next')
-            return redirect(next_page) if next_page else redirect(url_for('index'))
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('index'))
         else:
-            flash('Login Unsuccessful. Please check email and password', 'danger')
+            flash('Login Unsuccessful. Please check username and password', 'danger')
     return render_template('login.html', title='Login', form=form)
