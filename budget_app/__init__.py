77fafa6d7ab40460ecf33d2f0dc2f3e29f1c2491
munnytravels <munnawar.a.h@gmail.com>
2021-02-04 21:42:05 +0000

    login and logout work + front page stock photo added

--- budget_app/__init__.py
+++ budget_app/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '1705b0c63ae1efc33fa625f497da2739'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///budget.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 
 from budget_app import routes
--- budget_app/models.py
+++ budget_app/models.py
@@ -1,41 +1,36 @@
 from datetime import datetime
-from budget_app import db
+from budget_app import db, login_manager
 from flask_bcrypt import Bcrypt
+from flask_login import UserMixin
 
-class User(db.Model):
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     username = db.Column(db.String(20), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
-    budgets = db.relationship('Budget', backref='author', lazy=True)
+    budgets = db.relationship('Expense', backref='author', lazy=True)
     
     def __repr__(self):
         return f"User('{self.username}', {self.email}"
 
-class Budget(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    expense = db.Column(db.String(100), nullable=False)
-    expense_type = db.Column(db.Text, nullable=False)
-    balance_before = db.Column(db.Integer, nullable=False)
-    balance_after = db.Column(db.Integer, nullable=False)
-    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow) 
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    def __repr__(self):
-        return f"User('{self.username}', {self.email}"
-
-# class Budget(db.Model):
+# class Expense(db.Model):
 #     id = db.Column(db.Integer, primary_key=True)
-#     user_id = 
-#     start_date = 
-#     end_date = 
-#     start_balance =
-#     current_balance = db.Column(db.Integer, nullable=False)
-     
-
+#     vendor = db.Column(db.String(100), nullable=False)
+#     expense_type = db.Column(db.String(50), nullable=False)
+#     cost = db.Column(db.Integer, nullable=False)
+#     balance_before = db.Column(db.Integer, nullable=False)
+#     balance_after = db.Column(db.Integer, nullable=False)
+#     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow) 
+#     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 #     def __repr__(self):
 #         return f"User('{self.username}', {self.email}"
 
-# class Budget_Item(db.Model):
+
+# class Budget(db.Model):
 #     id = db.Column(db.Integer, primary_key=True)
 #     user_id = db.Column(db.String(100), nullable=False)
 #     budget_id = db.Column(db.Text, nullable=False)
