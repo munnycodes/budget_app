46b85e321c158ad98ba5776dfffb1aa6496da051
munnytravels <munnawar.a.h@gmail.com>
2021-02-23 15:39:11 +0000

    refactoring code;

--- budget_app/models.py
+++ budget_app/models.py
@@ -23,24 +23,9 @@ class Expense(db.Model):
     vendor = db.Column(db.String(100), nullable=False)
     expense_type = db.Column(db.String(50), nullable=False)
     cost = db.Column(db.Integer, nullable=False)
-#     balance_before = db.Column(db.Integer, nullable=False)
-#     balance_after = db.Column(db.Integer, nullable=False)
     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow) 
 
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     
     def __repr__(self):
         return f"Expense ('user_id', {self.user_id}), ('vendor', {self.vendor}), ('expense_type', {self.expense_type}), ('cost', {self.cost}), ('date_posted', {self.date_posted,'%d/%m/%y'})"
-
-
-# class Budget(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     user_id = db.Column(db.String(100), nullable=False)
-#     budget_id = db.Column(db.Text, nullable=False)
-#     amount = 
-#     category = 
-#     description = 
-#     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow) 
-
-#     def __repr__(self):
-#         return f"User('{self.username}', {self.email}"
