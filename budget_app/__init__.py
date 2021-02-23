d13410fd85f0323bbe748faa556ea3b80d298e95
munnytravels <70662658+munnytravels@users.noreply.github.com>
2021-02-23 19:32:21 +0000

    Update __init__.py

--- budget_app/__init__.py
+++ budget_app/__init__.py
@@ -9,7 +9,7 @@ from flask_datepicker import datepicker
 app = Flask(__name__)
 Bootstrap(app)
 datepicker(app)
-app.config['SECRET_KEY'] = '1705b0c63ae1efc33fa625f497da2739'
+app.config['SECRET_KEY'] = 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///budget.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
